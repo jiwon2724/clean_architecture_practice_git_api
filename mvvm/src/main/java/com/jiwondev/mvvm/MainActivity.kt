ae2594717568be5db8c3d90bd25a3831f8916ce8
jiwon2724 <stopone3119@naver.com>
2023-05-17 18:47:28 +0900

    [Feature]#1 feature : splash-screen 구현 완료

--- mvvm/src/main/java/com/jiwondev/mvvm/MainActivity.kt
+++ mvvm/src/main/java/com/jiwondev/mvvm/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.DialogFragment
 import com.jiwondev.mvvm.common.extension.startAnimationWithHide
 import com.jiwondev.mvvm.common.extension.startAnimationWithShow
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
